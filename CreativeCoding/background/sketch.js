f1f9856cc515e71d7e0bd4ab282b4d546d25ff57
ElliegHolden <144940456+ElliegHolden@users.noreply.github.com>
2024-01-05 11:00:59 -0500

    background

--- CreativeCoding/background/sketch.js
+++ CreativeCoding/background/sketch.js
@@ -1,6 +1,6 @@
 function setup() {//runs one time
 createCanvas (windowWidth, windowHeight);//canvas sized to window
-background (255, 0, 0);//red background
+background ('red');//red background
 }
 
 function draw() {//runs in a loop
