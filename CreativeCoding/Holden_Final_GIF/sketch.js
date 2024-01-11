435b273a8134ba8758af02ba4ce5af1022c9644e
ElliegHolden <144940456+ElliegHolden@users.noreply.github.com>
2024-01-11 13:40:04 -0500

    code

--- CreativeCoding/Holden_Final_GIF/sketch.js
+++ CreativeCoding/Holden_Final_GIF/sketch.js
@@ -1,17 +1,24 @@
 //create a program that enables us to customize the number of squares in a grid
 //two variables. 1) to choose the number of squares, 2) one that defines the side length based on the window width
+//previous code copied and then altered with GIF
+
+let gif;
+
+function preload (){
+gif = loadImage ('assets/Pink_Bows.gif');
+}
 
 function setup() {
   //runs one time
   createCanvas(windowWidth, windowHeight);
+  fill ('white');
   strokeWeight(1);
-  background (150);
-  fill ('magenta');
 }
 
 function draw() {
 //runs in a loop
 
+  background ('white');
   
   var num = 2//variable for the number of squares
   var sideLen = windowWidth/num;//variable for the side length of each square
@@ -20,12 +27,8 @@ function draw() {
    for(var y = 0; y < windowHeight; y = y + sideLen) {//loop to create a row of squares in the y direction
   
   for(var x = 0; x < windowWidth; x = x + sideLen) {//loop to create a grid in the y direction
-    quad (
-  x,y,
-  x + sideLen,y,
-  x + sideLen,y + sideLen,
-  x,y + sideLen
-);
+   
+   image (gif, x, y, sideLen, sideLen);
     
   }
   }
